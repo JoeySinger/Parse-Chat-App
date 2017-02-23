1c2d4f944144fe2b16d3b8f33dcea621a5399835
Joey Singer <joeysinger7@gmail.com>
2017-02-23 18:20:24 -0500

    Initial Commit

---
+++ ParseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseTest
+//
+//  Created by Joey Singer on 2/23/17.
+//  Copyright © 2017 Joey Singer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
